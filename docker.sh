1e1fc1b6cb80278be4783d1dd796880f53d6ee9b
Jonny Rodriguez <jojoroto@gmail.com>
2020-06-03 15:48:56 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common gpg-agent
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update

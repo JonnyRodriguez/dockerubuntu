f8afddc1dacb8c30e640c930ae190b6c5c75b3db
Jonny Rodriguez <jojoroto@gmail.com>
2020-06-08 17:23:39 -0400

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RELEASE=curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'
-sudo curl -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+RELEASE=$(curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+sudo curl -L https://github.com/docker/compose/releases/download/$RELEASE/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 echo $RELEASE

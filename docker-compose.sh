21adbde4379876601bdca2dcdc34a042a07fb3d1
Jonny Rodriguez <jojoroto@gmail.com>
2020-06-08 17:16:42 -0400

    Create docker-compose.sh

---
+++ docker-compose.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+RELEASE=curl --silent "https://api.github.com/repos/docker/compose/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'
+sudo curl -L https://github.com/docker/compose/releases/download/$RELEASE/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose

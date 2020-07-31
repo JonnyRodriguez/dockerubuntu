a28c0e3008d3bd4741752be59add4499d7662cee
Jonny Rodriguez <jojoroto@gmail.com>
2020-07-31 16:37:06 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,5 +6,5 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt update
 sudo apt install -y docker-ce
 sudo usermod -aG docker ${USER}
-sudo curl -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
